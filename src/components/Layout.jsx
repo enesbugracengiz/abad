909b1342facd51d00947a47aed3fda23a2cc9d55
Enes Buğra Cengiz <99767648+ebcengiz@users.noreply.github.com>
2025-08-06 22:19:34 +0300

    footer alanı kaldırıldı.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,10 @@
 import Header from "./Header";
-import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Header />
       <main className="main-content">{children}</main>
-      <Footer />
     </div>
   );
 };
